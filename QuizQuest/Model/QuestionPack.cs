34054e87411baafb609e3417f190fac43b806832
llavner <marcus.renvall@gmail.com>
2024-11-05 17:26:14 +0100

    Add project files.

---
+++ QuizQuest/Model/QuestionPack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizQuest.Model
+{
+    
+        enum Difficulty { Easy, Medium, Hard }
+
+        internal class QuestionPack
+        {
+
+            public string Name { get; set; }
+            public Difficulty Difficulty { get; set; }
+            public int TimeLimit { get; set; }
+            public List<Question> Questions { get; set; }
+
+            public QuestionPack(string name, Difficulty difficulty = Difficulty.Medium, int timeLimit = 30)
+            {
+                Name = name;
+                Difficulty = difficulty;
+                TimeLimit = timeLimit;
+                Questions = new List<Question>();
+            }
+
+
+
+        }
+
+
+    }
+
